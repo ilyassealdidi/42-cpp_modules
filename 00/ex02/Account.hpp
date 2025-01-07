f1fd0b9090025a35f52e20bf1835e1eb2e96fbeb
Ilyasse <ilyassealdidi@gmail.com>
2025-01-07 17:57:14 +0100

    a

---
+++ 00/ex02/Account.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 14:09:14 by ialdidi           #+#    #+#             */
+/*   Updated: 2025/01/07 14:46:54 by ialdidi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ACCOUNT_HPP
+#define ACCOUNT_HPP
+
+#include "iostream"
+
+class Account {
+    public :
+        static void displayAccountsInfos();
+};
+
+#endif
