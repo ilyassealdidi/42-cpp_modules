8a999b86dc1486107dce237d71905b19fb3cc2ba
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2025-01-08 20:54:23 +0100

    fix: correct output in megaphone.cpp

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 19:29:45 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/12/14 19:30:52 by ialdidi          ###   ########.fr       */
+/*   Updated: 2025/01/08 20:53:08 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 int main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
 		for (int j = 0; av[i][j] != 0; j++)
-			std::cout << std::toupper(av[i][j]) << std::endl;
+			std::cout << (char)std::toupper(av[i][j]);
 	}
+	std::cout << std::endl;
 	return 0;
 }
