ff402720a8be3a4d8bbd956d463f3bbe5358076a
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2025-01-08 21:52:46 +0100

    la chditk o dertlk n=-1

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 20:05:30 by ialdidi           #+#    #+#             */
-/*   Updated: 2025/01/08 20:11:04 by ialdidi          ###   ########.fr       */
+/*   Updated: 2025/01/08 21:46:20 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 int main() {
     int num = 5;
     Zombie* zombies = zombieHorde(num, "Zombie");
-    for (int i = 0; i < num; i++)
-        zombies[i].announce();
-    delete [] zombies;
+	if (zombies != nullptr)
+	{
+		for (int i = 0; i < num; i++)
+			zombies[i].announce();
+		delete [] zombies;
+	}
     return 0;
 }
--- 01/ex01/zombieHorde.cpp
+++ 01/ex01/zombieHorde.cpp
@@ -6,13 +6,15 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 20:05:36 by ialdidi           #+#    #+#             */
-/*   Updated: 2025/01/08 20:05:37 by ialdidi          ###   ########.fr       */
+/*   Updated: 2025/01/08 21:45:14 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(int N, std::string name) {
+	if (N <= 0)
+		return nullptr;
     Zombie* zombies = new Zombie[N];
     for (int i = 0; i < N; i++)
         zombies[i].setName(name);
