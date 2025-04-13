729337be87a1793e424b28ffcdeb1799d1a4f310
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2025-04-13 16:58:38 +0100

    Refactors dynamic_cast code and improves seeding logic
    Simplifies random number generation by removing redundant seeding check
    Restructures dynamic_cast logic to use nested try-catch blocks
    Improves code readability with consistent bracing style and formatting
    
    Reduces code duplication and maintains same functionality

--- 06/ex02/Base.cpp
+++ 06/ex02/Base.cpp
@@ -5,11 +5,7 @@
 
 Base *generate()
 {
-    static bool seeded = false;
-    if (!seeded) {
-        std::srand(std::time(NULL));
-        seeded = true;
-    }
+    std::srand(std::time(0));
     int random = std::rand() % 3;
 
     switch (random)
@@ -28,50 +24,39 @@ Base *generate()
 
 void identify(Base *p)
 {
-    if (dynamic_cast<A *>(p))
+    if (dynamic_cast<A *>(p)) {
         std::cout << "A" << std::endl;
-    else if (dynamic_cast<B *>(p))
+    } else if (dynamic_cast<B *>(p)) {
         std::cout << "B" << std::endl;
-    else if (dynamic_cast<C *>(p))
+    } else if (dynamic_cast<C *>(p)) {
         std::cout << "C" << std::endl;
-    else
+    } else {
         std::cout << "Unknown type" << std::endl;
+    }
 }
 
 void identify(Base &p)
 {
-    try
-    {
+    try {
         A &a = dynamic_cast<A &>(p);
         (void)a;
         std::cout << "A" << std::endl;
-        return;
     }
-    catch (const std::exception &)
-    {
-    }
-
-    try
-    {
-        B &b = dynamic_cast<B &>(p);
-        (void)b;
-        std::cout << "B" << std::endl;
-        return;
-    }
-    catch (const std::exception &)
-    {
-    }
-
-    try
-    {
-        C &c = dynamic_cast<C &>(p);
-        (void)c;
-        std::cout << "C" << std::endl;
-        return;
-    }
-    catch (const std::exception &)
-    {
-    }
-
-    std::cout << "Unknown type" << std::endl;
+    catch (const std::exception &) {
+        try {
+            B &b = dynamic_cast<B &>(p);
+            (void)b;
+            std::cout << "B" << std::endl;
+        }
+        catch (const std::exception &) {
+            try {
+                C &c = dynamic_cast<C &>(p);
+                (void)c;
+                std::cout << "C" << std::endl;
+            }
+            catch (const std::exception &) {
+                std::cout << "Unknown type" << std::endl;
+            }
+        }
+    }   
 }
--- 06/ex02/Base.hpp
+++ 06/ex02/Base.hpp
@@ -1,22 +1,18 @@
 #ifndef BASE_HPP
 #define BASE_HPP
 
-class Base
-{
+class Base {
     public:
         virtual ~Base() {}
 };
 
-class A : public Base
-{
+class A : public Base {
 };
 
-class B : public Base
-{
+class B : public Base {
 };
 
-class C : public Base
-{
+class C : public Base {
 };
 
 Base *generate(void);
