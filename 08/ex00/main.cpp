449ee4071ca46632d886dd59de5418bc54cd603f
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2025-04-14 20:51:20 +0100

    Refactor easyfind exception handling and improve formatting in main.cpp

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,28 +6,24 @@
 int main ()
 {
     std::vector<int> table;
-    try
-    {
+    try {
         table.push_back(1);
         table.push_back(8);
         table.push_back(3);
         easyfind(table, 5);
     }
-    catch (std::exception &e)
-    {
+    catch (std::exception &e) {
         std::cout << e.what() << std::endl;
     }
 
     std::list<int> list;
-    try
-    {
+    try {
         list.push_back(1);
         list.push_back(8);
         list.push_back(3);
         easyfind(list, -1);
     }
-    catch (std::exception &e)
-    {
+    catch (std::exception &e) {
         std::cout << e.what() << std::endl;
     }
     return 0;
