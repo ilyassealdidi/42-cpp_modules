9f5d6d0eedba066941b55123c760632e0b986a55
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2025-05-01 22:12:25 +0100

    Adding all the necessary functions

--- 09/ex00/BitcoinExchange.hpp
+++ 09/ex00/BitcoinExchange.hpp
@@ -3,20 +3,33 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdlib.h> // Try to replace this with <cstdlib> if possible
 #include <map>
 #include <ctime>
+
+typedef std::map<std::string, std::string> PriceMap;
 
 class BitcoinExchange
 {
     private:
+        PriceMap historicalData;
 
+        bool isFutureDate(const std::string& date) const;
+        bool isNumeric(const std::string &str) const;
+        bool isDate(const std::string &str) const;
+
+        std::string getClosestDate(const std::string& date) const;
+
+        void calculatePrice(const std::string& date, double amount) const;
     public:
         BitcoinExchange();
         BitcoinExchange(const BitcoinExchange& obj);
         BitcoinExchange& operator=(const BitcoinExchange& obj);
         ~BitcoinExchange();
 
-        // void	execute(std::string filename);
+        void getHitoricalPrices(std::string &filename);
+        
+        void printData(); //! To be removed in the final version
 };
 
 #endif
--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -1,19 +1,47 @@
-#include <iostream>
 #include "BitcoinExchange.hpp"
+#include <iostream>
+#include <cstdlib>
 
 int main(int ac, char **argv) {
-    if (ac != 2) {
-        std::cerr << "Invalid usage!" << std::endl;
-        std::cerr << "Usage: ./btc <filename>" << std::endl;
-        return 1;
+    std::map<std::string, std::string> data;
+    // data["2021-01-02"] = "43.43";
+    // data["2021-01-03"] = "44.44";
+    // data["2021-01-04"] = "45.45";
+    // data["2021-01-05"] = "46.46";
+    // data["2021-01-06"] = "47.47";
+    data["2021-01-07"] = "48.48";
+    data["2021-01-08"] = "49.49";
+    // data["2021-01-09"] = "50.50";
+
+    std::map<std::string, std::string>::iterator itr = data.upper_bound("2021-01-07");
+    if (itr == data.begin()) {
+        std::cout << "1" << std::endl;
     }
 
-    try {
-        BitcoinExchange btc;
-        // btc.execute(argv[1]);
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    if (itr == data.end()) {
+        std::cout << "2" << std::endl;
     }
+    
+    // std::cout << data.begin()->first << std::endl;
+    // std::cout << itr->first << std::endl;
+    //     // --itr;
+    //     std::cout << "Closest date: " << itr->first << ", Price: " << itr->second << std::endl;
+    // } else {
+    //     std::cout << "No previous date found." << std::endl;
+    // }
+    // if (ac != 2) {
+    //     std::cerr << "Invalid usage!" << std::endl;
+    //     std::cerr << "Usage: ./btc <filename>" << std::endl;
+    //     return EXIT_FAILURE;
+    // }
 
-    return 0;
+    // try {
+    //     BitcoinExchange btc;
+    //     btc.printData();
+    //     // btc.execute(argv[1]);
+    // } catch (const std::exception &e) {
+    //     std::cerr << e.what() << std::endl;
+    // }
+
+    return EXIT_SUCCESS;
 }
